2873eb62bbb35711d1ba68025b33686c06198b82
Jason Barden <jason.barden1@outlook.com>
2026-03-25 05:50:18 +0000

    chore: unrelated deletion of projects not required at the moment

--- packages/infrastructure/astar-dev-infrastructure-filesdb/AStar.Dev.Infrastructure.FilesDb/Models/ImageDetail.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace AStar.Dev.Infrastructure.FilesDb.Models;
-
-/// <summary>
-/// </summary>
-[Index(nameof(Width), nameof(Height))]
-public record ImageDetail(int? Width, int? Height);
