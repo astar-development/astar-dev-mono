6be5e7d47b5b6079b3ca1552c5cbdc01113aec01
Jason Barden <jason.barden1@outlook.com>
2026-04-01 19:44:08 +0100

    chore: interesting that the previous migration didn't include this....

---
+++ apps/desktop/AStar.Dev.OneDriveSync/Infrastructure/Persistence/Migrations/20260401184326_ClaudeUpdatesItDidntAddMigrationForPart2.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AStar.Dev.OneDriveSync.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClaudeUpdatesItDidntAddMigrationForPart2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserType",
+                table: "AppSettings",
+                type: "TEXT",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "Casual",
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserType",
+                table: "AppSettings",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 20,
+                oldDefaultValue: "Casual");
+        }
+    }
+}
