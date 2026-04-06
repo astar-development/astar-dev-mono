04dbaccbdcc177e722717dbcb28dd6ed27ea3392
Jason Barden <jason.barden1@outlook.com>
2026-04-06 12:26:37 +0100

    update paths to remove my username

--- packages/core/utilities/AStar.Dev.Utilities.Tests.Unit/ApplicationPathsProvider_should.cs
+++ packages/core/utilities/AStar.Dev.Utilities.Tests.Unit/ApplicationPathsProvider_should.cs
@@ -7,18 +7,18 @@ public class ApplicationPathsProvider_should
     public void return_the_expected_application_directory()
     {
         "test-application-name".ApplicationDirectory()
-            .ShouldBe("/home/jbarden/.config/test-application-name");
+            .ShouldEndWith("/.config/test-application-name");
     }
     [Fact]
     public void return_the_expected_logs_directory()
     {
         "test-application-name".LogsDirectory()
-            .ShouldBe("/home/jbarden/.local/share/test-application-name/logs");
+            .ShouldEndWith("/.local/share/test-application-name/logs");
     }
     [Fact]
     public void return_the_expected_users_directory()
     {
         "test-application-name".UserDirectory()
-            .ShouldBe("/home/jbarden/Documents/test-application-name/sync");
+            .ShouldEndWith("/Documents/test-application-name/sync");
     }
 }
