cda1dc19c7708f8ac2b25a73e5936582320b01df
Jason Barden <jason.barden1@outlook.com>
2026-04-29 13:59:12 +0100

    fix the domain name

--- apps/web/fab4kids/astro.config.ts
+++ apps/web/fab4kids/astro.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 import node from '@astrojs/node';
+import react from '@astrojs/react';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   integrations: [react()],
   adapter: node({ mode: 'standalone' }),
   output: 'static',
-  site: 'https://fab4kids.co.uk',
+  site: 'https://fab-4-kids.co.uk',
 });
