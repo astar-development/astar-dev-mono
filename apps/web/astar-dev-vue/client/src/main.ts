de5d69d015218a72f4c38d83c2f55488d8b225c1
Jay Barden <137005981+jaybarden1@users.noreply.github.com>
2026-04-29 14:05:13 +0100

    delete the old astar vue site (#180)

--- apps/web/astar-dev-vue/client/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './theme.css'
-import { useTheme } from './composables/useTheme'
-
-const { loadTheme } = useTheme()
-loadTheme()
-
-createApp(App).mount('#app')
