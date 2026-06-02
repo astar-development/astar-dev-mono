d4650a3f52ee7ed4e55f3eda6971e7f0a09d965d
Jason Barden <jason.barden1@outlook.com>
2026-06-02 12:57:18 +0100

    feat(apps/desktop): implement classification spike with EF Core and SQLite integration

--- apps/desktop/spike/ClassificationSpike/Program.cs
+++ apps/desktop/spike/ClassificationSpike/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using AStar.Dev.OneDrive.Sync.Client.Data;
+using AStar.Dev.OneDrive.Sync.Client.Infrastructure;
+using AStar.Dev.Utilities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceProvider = BuildServiceProvider();
+using var dbContext = serviceProvider.GetRequiredService<AppDbContext>();
+
+dbContext.SyncedItems.Where(x => !x.IsFolder).Take(10).ToList()
+    .ForEach(item => Console.WriteLine($"Synced Item: {item.Id}, {item.RemotePath}"));
+
+DisplayFileCount(dbContext);
+
+static void ConfigureDbContext(DbContextOptionsBuilder builder)
+{
+    string dbPath = ApplicationMetadata.ApplicationNameHyphenated.ApplicationDirectory().CombinePath($"{ApplicationMetadata.ApplicationNameHyphenated}.db");
+    _ = builder.UseSqlite($"Data Source={dbPath}");
+}
+
+static ServiceProvider BuildServiceProvider()
+{
+    var serviceCollection = new ServiceCollection();
+    serviceCollection.AddDbContext<AppDbContext>(options => ConfigureDbContext(options));
+    var serviceProvider = serviceCollection.BuildServiceProvider();
+    return serviceProvider;
+}
+
+static void DisplayFileCount(AppDbContext dbContext)
+{
+    int count = dbContext.SyncedItems.Count(x => !x.IsFolder);
+    Console.WriteLine($"Count of non-folder synced items: {count}");
+}
