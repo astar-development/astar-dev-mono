1d46d0d8f649276180b1aea84058fdfd70d999ce
Jason Barden <jason.barden1@outlook.com>
2026-06-16 15:06:36 +0100

    fix(configuration): add default constructor to ClientConfiguration

--- apps/desktop/AStar.Dev.OneDrive.Sync.Client/Infrastructure/ApplicationConfiguration/ClientConfiguration.cs
+++ apps/desktop/AStar.Dev.OneDrive.Sync.Client/Infrastructure/ApplicationConfiguration/ClientConfiguration.cs
@@ -2,5 +2,10 @@ namespace AStar.Dev.OneDrive.Sync.Client.Infrastructure.ApplicationConfiguration
 
 public record ClientConfiguration(string ApplicationName, string ApplicationVersion)
 {
+    public ClientConfiguration() : this(string.Empty, string.Empty)
+    {
+
+    }
+
     internal static string SectionName => "AStarDevOneDriveClient";
 }
