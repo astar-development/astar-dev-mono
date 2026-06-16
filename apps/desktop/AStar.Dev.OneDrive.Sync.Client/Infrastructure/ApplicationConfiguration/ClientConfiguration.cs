347e17abd8e612c7984dbc52554b27b2e397ddba
Jason Barden <jason.barden1@outlook.com>
2026-06-16 13:08:50 +0100

    refactor(configuration): update configuration binding to use static section names

--- apps/desktop/AStar.Dev.OneDrive.Sync.Client/Infrastructure/ApplicationConfiguration/ClientConfiguration.cs
+++ apps/desktop/AStar.Dev.OneDrive.Sync.Client/Infrastructure/ApplicationConfiguration/ClientConfiguration.cs
@@ -2,10 +2,5 @@ namespace AStar.Dev.OneDrive.Sync.Client.Infrastructure.ApplicationConfiguration
 
 public record ClientConfiguration(string ApplicationName, string ApplicationVersion)
 {
-    public ClientConfiguration() : this(string.Empty, string.Empty)
-    {
-
-    }
-
     internal static string SectionName => "AStarDevOneDriveClient";
 }
