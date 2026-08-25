7cf5aa8b879952540f803c2de66dc2b5aba3627c
Jason Barden <jason.barden@outlook.com>
2026-08-25 20:32:47 +0100

    test(tests-integration): ensure database is created and deleted in GivenAControlDbContext test(tests-unit): move DateTimeFormatRegex to RegexHelpers and clean up GivenAScrapeConfigurationEntity

--- packages/AStarDev.ControlDb/AStarDev.ControlDb.TestsUnit/ScrapeConfiguration/GivenAScrapeConfigurationEntity.cs
+++ packages/AStarDev.ControlDb/AStarDev.ControlDb.TestsUnit/ScrapeConfiguration/GivenAScrapeConfigurationEntity.cs
@@ -1,6 +1,6 @@
-using System.Text.RegularExpressions;
 using AStarDev.Utilities;
 using AStarDev.ControlDb.TestsUnit.TestDataFactories;
+using static AStarDev.ControlDb.TestsUnit.Utilities.RegexHelpers;
 
 namespace AStarDev.ControlDb.TestsUnit.ScrapeConfiguration;
 
@@ -12,7 +12,4 @@ public partial class GivenAScrapeConfigurationEntity
         string sut = ScrapeConfigurationEntityFactory.CreateScrapeConfigurationEntity().ToJson() + Environment.NewLine;
         sut.ShouldMatchApproved(c => c.WithScrubber(s => DateTimeFormatRegex().Replace(s, "<date>")));
     }
-
-    [GeneratedRegex(@"\d{1,4}-\d{1,2}-\d{1,2}T\d{1,2}:\d{1,2}:\d{1,2}\.\d{1,7}\+00:00")]
-    private static partial Regex DateTimeFormatRegex();
 }
